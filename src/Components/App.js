4f25be6614d9a3601e3ba10e4f2b9ba86504bcab
Miguel Faria <mattar1704miguel@gmail.com>
2022-07-19 10:55:02 -0300

    exibição tela inicial

---
+++ src/Components/App.js
@@ -0,0 +1,10 @@
+import LandingPage from "./LandingPage";
+
+
+export default function App() {
+    return (
+        <div>
+            <LandingPage />
+        </div>
+    ); 
+}
---
+++ src/Components/LandingPage.js
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+    return (
+        <div className="landing-page">
+            <img src="assets/logo.png" alt="Logo ZapRecall"/>
+            <h1>ZapRecall</h1>
+            <input type="button" value="Iniciar Recall!"></input>
+        </div>
+    );
+}
